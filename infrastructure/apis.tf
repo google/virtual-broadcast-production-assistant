93a85d87cc6b156051635648fc24cadbc6dd531d
Justin Grayston <jgrayston@google.com>
2025-02-09 13:36:58 +0000

    Local Initial Commit

---
+++ infrastructure/apis.tf
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2023 Google LLC
+ */
+
+resource "google_project_service" "enable-cloud-resource-manager-api" {
+
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+  }
+
+  disable_dependent_services = false
+  disable_on_destroy         = false
+
+}
+
+resource "google_project_service" "enable-required-apis" {
+
+  for_each = toset([
+    "cloudresourcemanager.googleapis.com",
+    "monitoring.googleapis.com",
+    "run.googleapis.com",
+    "aiplatform.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "servicenetworking.googleapis.com",
+  ])
+  project = var.project_id
+  service = each.value
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+  }
+
+  disable_dependent_services = false
+  disable_on_destroy         = false
+
+  depends_on = [
+    google_project_service.enable-cloud-resource-manager-api
+  ]
+
+}
---
+++ infrastructure/main.tf
@@ -0,0 +1,81 @@
+/*
+ Copyright 2025 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0" # Or latest compatible version
+    }
+  }
+
+
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    prefix = "terraform"
+  }
+}
+
+module "backend_service_account" {
+  source       = "./modules/backend/service-accounts"
+  name         = "backend-sa"
+  display_name = "Backend Service Account"
+}
+
+# module "backend_secrets" {
+#   source     = "./modules/secret-manager"
+#   project_id = var.project_id
+#   secrets = {
+#     GOOGLE_API_KEY = {
+#       value = var.gemini_api_key # Pass this as a variable
+#     },
+#     OPENWEATHER_API_KEY = {           # Include other secrets similarly
+#       value = var.openweather_api_key # Pass this as a variable
+#     }
+#     # Add other secrets as needed...
+#   }
+# }
+
+
+# resource "google_project_iam_member" "pastra_backend_secret_accessor" {
+#   project = var.project_id
+#   role    = "roles/secretmanager.secretAccessor"
+#   member  = "serviceAccount:${module.pastra_backend_service_account.email}"
+# }
+
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "The GCP region for Cloud Run"
+  default     = "europe-west1"
+}
+
+variable "terraform_bucket" {
+  type        = string
+  description = "Terraform bucket name"
+}
