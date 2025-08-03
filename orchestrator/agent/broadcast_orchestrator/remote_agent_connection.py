f8ec0afd9c53d33cdbcfdaab27f0c643d965d8ac
Justin Grayston <jgrayston@google.com>
2025-08-03 19:31:30 +0100

    This adds a simple static frontend and a separet Runner
    It also adds multiple agents and agent calling.

--- orchestrator/agent/broadcast_orchestrator/remote_agent_connection.py
+++ orchestrator/agent/broadcast_orchestrator/remote_agent_connection.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import logging
 
 from typing import Callable
 
@@ -29,6 +30,8 @@ from a2a.types import (
 )
 from dotenv import load_dotenv
 
+logger = logging.getLogger(__name__)
+
 load_dotenv()
 
 TaskCallbackArg = Task | TaskStatusUpdateEvent | TaskArtifactUpdateEvent
@@ -36,23 +39,23 @@ TaskUpdateCallback = Callable[[TaskCallbackArg, AgentCard], Task]
 
 
 class RemoteAgentConnections:
-  """A class to hold the connections to the remote agents."""
+    """A class to hold the connections to the remote agents."""
 
-  def __init__(self, agent_card: AgentCard, agent_url: str):
-    print(f"agent_card: {agent_card}")
-    print(f"agent_url: {agent_url}")
-    self._httpx_client = httpx.AsyncClient(timeout=30)
-    self.agent_client = A2AClient(self._httpx_client,
-                                  agent_card,
-                                  url=agent_url)
-    self.card = agent_card
-    self.conversation_name = None
-    self.conversation = None
-    self.pending_tasks = set()
+    def __init__(self, agent_card: AgentCard, agent_url: str):
+        logger.info("agent_card: %s", agent_card)
+        logger.info("agent_url: %s", agent_url)
+        self._httpx_client = httpx.AsyncClient(timeout=30)
+        self.agent_client = A2AClient(self._httpx_client,
+                                      agent_card,
+                                      url=agent_url)
+        self.card = agent_card
+        self.conversation_name = None
+        self.conversation = None
+        self.pending_tasks = set()
 
-  def get_agent(self) -> AgentCard:
-    return self.card
+    def get_agent(self) -> AgentCard:
+        return self.card
 
-  async def send_message(
-      self, message_request: SendMessageRequest) -> SendMessageResponse:
-    return await self.agent_client.send_message(message_request)
+    async def send_message(
+            self, message_request: SendMessageRequest) -> SendMessageResponse:
+        return await self.agent_client.send_message(message_request)
