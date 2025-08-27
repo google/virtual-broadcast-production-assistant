5580419a34a47edba1eb3481275c567831192a8d
Justin Grayston <jgrayston@google.com>
2025-08-27 21:12:46 +0100

    Route agent-health-checker egress via VPC
    Also
    
    * Ignore container versions for orchestrator and agent health checker
    * Add mamanagement of IAM for Cloud BUild runner under terraform

--- infrastructure/modules/orchestrator/outputs.tf
+++ infrastructure/modules/orchestrator/outputs.tf
@@ -12,3 +12,8 @@ output "nat_ip_address" {
   description = "The static outbound IP address of the Cloud Run service."
   value       = google_compute_address.nat_ip.address
 }
+
+output "vpc_access_connector_id" {
+  description = "The ID of the VPC Access Connector."
+  value       = google_vpc_access_connector.connector.id
+}
